518d50d1fffeda560387ebe8f543bbfa18666da4
Golla Geetha <gollasrinu122@gmail.com>
2025-04-03 14:42:09 +0530

    uploading project files

---
+++ app.py
@@ -0,0 +1,164 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import joblib
+import sqlite3
+
+import numpy as np
+import pandas as pd
+from sklearn import metrics 
+import warnings
+import pickle
+import pandas as pd
+import numpy as np
+import pickle
+import sqlite3
+import random
+
+import smtplib 
+from email.message import EmailMessage
+from datetime import datetime
+
+warnings.filterwarnings('ignore')
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route('/home1')
+def home1():
+	return render_template('home1.html')
+
+
+@app.route('/logon')
+def logon():
+	return render_template('signup.html')
+
+@app.route('/login')
+def login():
+	return render_template('signin.html')
+
+
+@app.route('/home')
+def home():
+	return render_template('home.html')
+
+
+@app.route("/signup")
+def signup():
+    global otp, username, name, email, number, password
+    username = request.args.get('user','')
+    name = request.args.get('name','')
+    email = request.args.get('email','')
+    number = request.args.get('mobile','')
+    password = request.args.get('password','')
+    otp = random.randint(1000,5000)
+    print(otp)
+    msg = EmailMessage()
+    msg.set_content("Your OTP is : "+str(otp))
+    msg['Subject'] = 'OTP'
+    msg['From'] = "myprojectstp@gmail.com"
+    msg['To'] = email
+    
+    
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("myprojectstp@gmail.com", "paxgxdrhifmqcrzn")
+    s.send_message(msg)
+    s.quit()
+    return render_template("val.html")
+
+@app.route('/predict_lo', methods=['POST'])
+def predict_lo():
+    global otp, username, name, email, number, password
+    if request.method == 'POST':
+        message = request.form['message']
+        print(message)
+        if int(message) == otp:
+            print("TRUE")
+            con = sqlite3.connect('signup.db')
+            cur = con.cursor()
+            cur.execute("insert into `info` (`user`,`email`, `password`,`mobile`,`name`) VALUES (?, ?, ?, ?, ?)",(username,email,password,number,name))
+            con.commit()
+            con.close()
+            return render_template("signin.html")
+    return render_template("signup.html")
+
+@app.route("/signin")
+def signin():
+
+    mail1 = request.args.get('user','')
+    password1 = request.args.get('password','')
+    con = sqlite3.connect('signup.db')
+    cur = con.cursor()
+    cur.execute("select `user`, `password` from info where `user` = ? AND `password` = ?",(mail1,password1,))
+    data = cur.fetchone()
+
+    if data == None:
+        return render_template("signin.html")    
+
+    elif mail1 == str(data[0]) and password1 == str(data[1]):
+        return render_template("home.html")
+    else:
+        return render_template("signin.html")
+
+@app.route("/notebook1")
+def notebook1():
+    return render_template("Yield.html")
+
+@app.route("/notebook2")
+def notebook2():
+    return render_template("Fertilizer.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features= [float(x) for x in request.form.values()]
+    print(int_features,len(int_features))
+    final4=[np.array(int_features)]
+    model = joblib.load('model_yield.sav')
+    predict = model.predict(final4)
+
+    
+    output = 'The Crop Yield is Predicted as ' + str(predict[0]) + ' production per unit area!' 
+    
+    
+    return render_template('prediction.html', output=output)
+
+
+@app.route('/predict1',methods=['POST'])
+def predict1():
+    int_features= [float(x) for x in request.form.values()]
+    print(int_features,len(int_features))
+    final4=[np.array(int_features)]
+    model = joblib.load('model_fertilizer.sav')
+    predict = model.predict(final4)
+
+    if predict == 0:
+        output = "Recommendation Fertilizer is 10-26-26!"
+    elif predict == 1:
+        output = "Recommendation Fertilizer is 14-35-14!"
+    elif predict == 2:
+        output = "Recommendation Fertilizer is 17-17-17!"
+    elif predict == 3:
+        output = "Recommendation Fertilizer is 20-20!"
+    elif predict == 4:
+        output = "Recommendation Fertilizer is 28-28!"
+    elif predict == 5:
+        output = "Recommendation Fertilizer is DAP!"
+    elif predict == 6:
+        output = "Recommendation Fertilizer is Urea!"
+    
+    
+    return render_template('prediction.html', output=output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
